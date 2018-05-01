eac491b0b43a832ed61605acf6922fc25a9c2732
Mindaugas Varaska <mvaras2@uic.edu>
2018-05-01 22:33:10 +0000

    2.1 finished

--- Ex_Files_React_Designers/Exercise Files/Ch02/02_01/start/app.js
+++ Ex_Files_React_Designers/Exercise Files/Ch02/02_01/start/app.js
@@ -1,7 +1,7 @@
 (function() {
   "use strict";
 
-  var ProductCostumizer = React.createElement();
+  var ProductCostumizer = React.createElement('div', { className: "customizer" }, "Product customizer will go here");
   
   ReactDOM.render(ProductCostumizer, document.getElementById('react-root'));
 
