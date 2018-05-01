b2f730428279557a5c78a8a31d0d2fb68e1f2a71
Mindaugas Varaska <mvaras2@uic.edu>
2018-05-01 23:03:53 +0000

    Refactured into babel app.js

--- Ex_Files_React_Designers/Exercise Files/Ch02/02_04/start/app.js
+++ Ex_Files_React_Designers/Exercise Files/Ch02/02_04/start/app.js
@@ -1,27 +1,24 @@
 (function() {
   "use strict";
 
+
+
   function ProductImage(props) {
-    return React.createElement("img", {
-      src: "../../../assets/red.jpg",
-      alt: "Product Image"
-    });
+    return <img src="../../../assets/red.jpg" alt= "Product Image" />;
   }
 
   function ProductCustomizer(props) {
-    return React.createElement(
-      "div",
-      { className: "customizer" },
-      React.createElement(
-        "div",
-        { className: "product-image" },
-        React.createElement(ProductImage)
-      )
+    
+    return (
+      <div className="customizer">
+        <div className="product-image">
+          <ProductImage />
+        </div>
+      </div>  
     );
   }
 
   ReactDOM.render(
-    React.createElement(ProductCustomizer),
-    document.getElementById("react-root")
+    <ProductCustomizer />, document.getElementById("react-root")
   );
 })();
